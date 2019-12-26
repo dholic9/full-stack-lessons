19e2530d72bac29b7d98f58e8ad5a1960c2bec62
david <dholic9@github.com>
2019-12-26 20:26:43 +0000

    completed

--- exercises/array-map/main.js
+++ exercises/array-map/main.js
@@ -21,3 +21,46 @@ console.log('upperCased: ', upperCased);
 
 const firstLetters = languages.map(word => word[0]);
 console.log('firstLetters: ', firstLetters);
+
+function biggerThanTen(number) {
+  return number > 10;
+}
+
+const numberBiggerThan10 = numbers.filter(biggerThanTen);
+console.log('numberBiggerThan10: ', numberBiggerThan10);
+
+function biggerThan7(input) {
+  return input.length > 7;
+}
+const wordBiggerThan7 = languages.filter(biggerThan7);
+console.log('wordBiggerThan7: ', wordBiggerThan7);
+
+function dividedBy2(number) {
+  return number / 2;
+}
+const numberDividedBy2 = numbers.map(dividedBy2);
+console.log('numberDividedBy2:', numberDividedBy2);
+
+function filter(array, predicate) {
+  const filteredArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (predicate(array[i])) {
+      filteredArray.push(array[i]);
+    }
+  }
+  return filteredArray;
+}
+
+const largerThan3 = filter(numbers, number => number > 3);
+console.log('largerThan3: ', largerThan3);
+
+function map(array, transform) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(transform(array[i]));
+  }
+  return newArray;
+}
+
+const multiplyBy3 = map(numbers, number => number * 3);
+console.log('multiplyBy3: ', multiplyBy3);
