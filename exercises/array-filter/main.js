6704d71a6424beb39aa3a9cd01c9453ea6113b98
david <dholic9@github.com>
2019-12-26 19:33:36 +0000

    completed

--- exercises/array-filter/main.js
+++ exercises/array-filter/main.js
@@ -14,3 +14,15 @@ const names = [
   'Andy',
   'Tim D.'
 ];
+
+const evenNumbers = numbers.filter(number => (number % 2) === 0);
+console.log('evenNumbers: ', evenNumbers);
+
+const overFive = numbers.filter(number => number > 5);
+console.log('overFive: ', overFive);
+
+const startWithT = names.filter(name => name.startsWith('T'));
+console.log('startWithT: ', startWithT);
+
+const haveD = names.filter(name => name.includes('D' || 'd'));
+console.log('haveD: ', haveD);
