6262436f7dce3a12441f747b36bc367487b51dfa
david <dholic9@github.com>
2020-01-03 04:42:18 +0000

    completed

--- exercises/fetch/main.js
+++ exercises/fetch/main.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-console */
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+  .then(response => {
+    return response.json();
+  })
+  .then(dataJson => {
+    console.log(dataJson);
+  });
